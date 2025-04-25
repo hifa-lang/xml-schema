0dea90e797ae791b9a695b0b651a0b76088d944f
kurotakazuki <kabisakanakabi@gmail.com>
2025-04-25 23:21:00 +0100

    add: test complex type sequence choice

--- xml_schema/tests/complex_type.rs
+++ xml_schema/tests/complex_type.rs
@@ -1,5 +1,6 @@
 use hifa_xml_schema_derive::XmlSchema;
 use hifa_yaserde::de::from_str;
+use hifa_yaserde::ext::Boxed;
 use hifa_yaserde::ser::to_string;
 
 #[test]
@@ -28,5 +29,44 @@ fn complex_type_string() {
   assert_eq!(
     data,
     r#"<?xml version="1.0" encoding="UTF-8"?><ComplexListOfElements><Annotation>Test content</Annotation><Label>Label content</Label></ComplexListOfElements>"#
+  );
+}
+
+#[test]
+fn complex_type_sequence_choice() {
+  #[derive(Debug, XmlSchema)]
+  #[xml_schema(source = "tests/complex_type_sequence_choice.xsd")]
+  struct ComplexTypeSchema;
+
+  let xml_1 = r#"
+  <Animals>
+    <Cat name="AAA" />
+    <Dog name="BBB" />
+    <Cat name="CCC" />
+  </Animals>
+  "#;
+
+  let sample_1: xml_schema_types::Animals = from_str(xml_1).unwrap();
+
+  let model = xml_schema_types::Animals {
+    cat_list: vec![
+      Boxed::from(xml_schema_types::Cat {
+        name: "AAA".to_string(),
+      }),
+      Boxed::from(xml_schema_types::Cat {
+        name: "CCC".to_string(),
+      }),
+    ],
+    dog_list: vec![Boxed::from(xml_schema_types::Dog {
+      name: "BBB".to_string(),
+    })],
+  };
+
+  assert_eq!(sample_1, model);
+
+  let data = to_string(&model).unwrap();
+  assert_eq!(
+    data,
+    r#"<?xml version="1.0" encoding="UTF-8"?><Animals><Cat name="AAA" /><Cat name="CCC" /><Dog name="BBB" /></Animals>"#
   );
 }
